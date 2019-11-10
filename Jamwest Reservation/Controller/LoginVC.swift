40f0fae440ac7306056d9c81b9c7f6900f4c48f9
Shannon Robinson <Srobinson@wurllink.com>
2019-11-10 11:00:21 -0500

    Edit fireBase file to remove error message

--- Jamwest Reservation/Controller/LoginVC.swift
+++ Jamwest Reservation/Controller/LoginVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginVC: UIViewController {
 
@@ -19,6 +20,7 @@ class LoginVC: UIViewController {
        textfield.borderStyle = .roundedRect
        textfield.font = UIFont.systemFont(ofSize: 18)
        textfield.textColor = .black
+       textfield.addTarget(self, action: #selector(formValidation), for: .editingChanged)
        return textfield
    }()
     
@@ -26,10 +28,12 @@ class LoginVC: UIViewController {
        
         let textfield = UITextField()
         textfield.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray])
+        textfield.isSecureTextEntry = true
         textfield.backgroundColor = UIColor.white
         textfield.borderStyle = .roundedRect
         textfield.font = UIFont.systemFont(ofSize: 18)
         textfield.textColor = .black
+        textfield.addTarget(self, action: #selector(formValidation), for: .editingChanged)
         return textfield
     }()
     
@@ -38,8 +42,10 @@ class LoginVC: UIViewController {
         button.setTitle("Login", for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 24)
         button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = UIColor(red: 242/255, green: 125/255, blue: 15/255, alpha: 1)
+        button.backgroundColor = UIColor(red: 374/255, green: 175/255, blue: 22/255, alpha: 1)
         button.layer.cornerRadius = 5
+        button.isEnabled = false
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
         return button
     }()
     
@@ -48,9 +54,8 @@ class LoginVC: UIViewController {
         
         let attributedTitle = NSMutableAttributedString(string: "Dont have an account?  ", attributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 18), NSAttributedString.Key.foregroundColor: UIColor.lightGray])
         attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 18), NSAttributedString.Key.foregroundColor: UIColor(red: 242/255, green: 125/255, blue: 15/255, alpha: 1)]))
-        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
         button.setAttributedTitle(attributedTitle, for: .normal)
-        
         return button
     }()
     
@@ -61,16 +66,54 @@ class LoginVC: UIViewController {
         view.backgroundColor = UIColor.init(displayP3Red: 17/255, green: 16/255, blue: 38/255, alpha: 95)
         
         navigationController?.navigationBar.isHidden = true
-        
         configureViewComponents()
         
         view.addSubview(dontHaveAccountButton)
         dontHaveAccountButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 30, paddingRight: 0, width: 0, height: 50)
     }
    
-    @objc func handleSignUp() {
+    @objc func handleShowSignUp() {
         let signUpVC = SignUpVC()
+        clearTextFields()
+        
         navigationController?.pushViewController(signUpVC, animated: true)
+    }
+    
+    @objc func handleLogin() {
+        guard let email = emailTextField.text,
+              let password = passwordTextField.text else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            
+//            handle error
+            if let error = error {
+                print("Unable to sign user in with error", error.localizedDescription)
+                return
+            }
+            
+//            handle success
+            print("Successfully signed user in")
+            self.clearTextFields()
+        } 
+    }
+    
+    @objc func formValidation() {
+           guard
+           emailTextField.hasText,
+           passwordTextField.hasText else {
+           loginButton.isEnabled = false
+           loginButton.backgroundColor = UIColor(red: 374/255, green: 175/255, blue: 22/255, alpha: 1)
+           return
+           }
+           loginButton.isEnabled = true
+           loginButton.backgroundColor = UIColor(red: 242/255, green: 125/255, blue: 15/255, alpha: 1)
+       }
+    
+    func clearTextFields() {
+        
+        emailTextField.text = nil
+        passwordTextField.text = nil
+        formValidation()
     }
     
     func configureViewComponents() {
--- Jamwest Reservation/Controller/SignUpVC.swift
+++ Jamwest Reservation/Controller/SignUpVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpVC: UIViewController {
     
@@ -18,6 +19,7 @@ class SignUpVC: UIViewController {
           textfield.borderStyle = .roundedRect
           textfield.font = UIFont.systemFont(ofSize: 18)
           textfield.textColor = .black
+          textfield.addTarget(self, action: #selector(formValidation), for: .editingChanged)
           return textfield
       }()
     
@@ -29,6 +31,7 @@ class SignUpVC: UIViewController {
           textfield.borderStyle = .roundedRect
           textfield.font = UIFont.systemFont(ofSize: 18)
           textfield.textColor = .black
+          textfield.addTarget(self, action: #selector(formValidation), for: .editingChanged)
           return textfield
       }()
     
@@ -36,10 +39,12 @@ class SignUpVC: UIViewController {
          
           let textfield = UITextField()
           textfield.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor : UIColor.gray])
+          textfield.isSecureTextEntry = true
           textfield.backgroundColor = UIColor.white
           textfield.borderStyle = .roundedRect
           textfield.font = UIFont.systemFont(ofSize: 18)
           textfield.textColor = .black
+          textfield.addTarget(self, action: #selector(formValidation), for: .editingChanged)
           return textfield
       }()
     
@@ -48,8 +53,10 @@ class SignUpVC: UIViewController {
         button.setTitle("Sign Up", for: .normal)
         button.titleLabel?.font = .boldSystemFont(ofSize: 24)
         button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = UIColor(red: 242/255, green: 125/255, blue: 15/255, alpha: 1)
+        button.backgroundColor = UIColor(red: 374/255, green: 175/255, blue: 22/255, alpha: 1)
         button.layer.cornerRadius = 5
+        button.isEnabled = false
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
         return button
     }()
     
@@ -76,12 +83,63 @@ class SignUpVC: UIViewController {
     }
     
     @objc func handleShowLogIn() {
+        
+        clearTextFields()
+        
         _ = navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func handleSignUp() {
+        
+        guard let email = emailTextField.text,
+              let password = passwordTextfield.text,
+              let username = userNameTextfield.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+//            handle error
+            if let error = error {
+                print("Failed to create user with error:", error.localizedDescription)
+                return
+            }
+            
+            guard let uid = user?.user.uid else { return }
+            
+            let dictionaryValues = ["username": username]
+            let values = [uid: dictionaryValues]
+            
+//            save user info to dataBase
+            Database.database().reference().child("users").updateChildValues(values) { (error, ref) in
+                print("Successfully created user and saver information to datatbase")
+                self.clearTextFields()
+            }
+        }
+        
+    }
+    
+    @objc func formValidation() {
+        
+        guard
+            emailTextField.hasText,
+            userNameTextfield.hasText,
+            passwordTextfield.hasText else {
+                signUpButton.isEnabled = false
+                signUpButton.backgroundColor = UIColor(red: 374/255, green: 175/255, blue: 22/255, alpha: 1)
+                return
+        }
+        signUpButton.isEnabled = true
+        signUpButton.backgroundColor = UIColor(red: 242/255, green: 125/255, blue: 15/255, alpha: 1)
+    }
+    
+    func clearTextFields() {
+        
+        emailTextField.text = nil
+        userNameTextfield.text = nil
+        passwordTextfield.text = nil
+        formValidation()
     }
 
     func configureViewComponents() {
         let stackView = UIStackView(arrangedSubviews: [emailTextField,userNameTextfield,passwordTextfield,signUpButton])
-        
         stackView.axis = .vertical
         stackView.spacing = 15
         stackView.distribution = .fillEqually
